e09c243ca43546af28c538a64f314d24ddcd5549
Philipp Ma <philipp.majun@gmail.com>
2022-06-16 17:32:07 +0200

    Add Jest/Playwright yml files

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,10 +2,18 @@
 import { PlaywrightTestConfig, devices } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
+  webServer: {
+    command: 'yarn start',
+    port: 3000,
+    reuseExistingServer: !process.env.CI,
+  },
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
+  reporter: process.env.CI ? 'list' : 'html',
   use: {
     trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
   },
   // Ignore JEST test files
   testIgnore: '**/util/__tests__/**',
