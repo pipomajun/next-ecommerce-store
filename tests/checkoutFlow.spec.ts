c39ffff74023d528067853a46e55aa31d6fa7d49
Philipp Ma <philipp.majun@gmail.com>
2022-06-17 14:39:03 +0200

    Change test-playwright.yml

--- tests/checkoutFlow.spec.ts
+++ tests/checkoutFlow.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 const baseUrl = 'http://localhost:3000/';
 test('test the checkout flow', async ({ page }) => {
